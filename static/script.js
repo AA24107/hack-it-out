6e018eeac1d39e03bb3844f6c116b518740b4f47
Afeefa Ansari <ansariafeefa7860@gmail.com>
2026-01-12 20:29:09 +0530

    Added loading state

--- static/script.js
+++ static/script.js
@@ -13,39 +13,52 @@ locationFilter.addEventListener("change", filterCards);
 availabilityFilter.addEventListener("change", filterCards);
 
 function filterCards() {
-    const searchTerm = searchInput.value.toLowerCase();
-    const words = searchTerm.split(' ');
-    const category = categoryFilter.value;
-    const location = locationFilter.value;
-    const availability = availabilityFilter.value;
 
-    for (const card of cards) {
-        const cardText = card.textContent.toLowerCase();
+    loading.style.display = 'block';
+    let cardCount = 0;
 
-        let show = true;
+    setTimeout(function() {
+        const searchTerm = searchInput.value.toLowerCase();
+        const words = searchTerm.split(' ');
+        const category = categoryFilter.value;
+        const location = locationFilter.value;
+        const availability = availabilityFilter.value;
 
-        //Search bar
-        for (const word of words) {
-            if (!cardText.includes(word)) {
+        for (const card of cards) {
+            const cardText = card.textContent.toLowerCase();
+
+            let show = true;
+
+            //Search bar
+            for (const word of words) {
+                if (!cardText.includes(word)) {
+                    show = false;
+                    break;
+                }
+            }
+
+            //Category filter
+            if (category !== 'all' && !cardText.includes(category.toLowerCase())) {
                 show = false;
-                break;
+            }
+
+            //Location filter
+            if (location !== 'all' && !cardText.includes(location.toLowerCase())) {
+                show = false;
+            }
+
+            //Availability filter
+            if (availability !== 'all' && !cardText.includes(availability.toLowerCase())) {
+                show = false;
+            }
+            card.style.display = show ? 'block' : 'none';
+            if (show) {
+                cardCount++;
             }
         }
+        loading.style.display = 'none';
+        cardCount = 0 ? 'block' : 'none';
+        }, 1000);
 
-        //Category filter
-        if (category !== 'all' && !cardText.includes(category.toLowerCase())) {
-            show = false;
-        }
-
-        //Location filter
-        if (location !== 'all' && !cardText.includes(location.toLowerCase())) {
-            show = false;
-        }
-
-        //Availability filter
-        if (availability !== 'all' && !cardText.includes(availability.toLowerCase())) {
-            show = false;
-        }
-        card.style.display = show ? 'block' : 'none';
     }
-}
+
